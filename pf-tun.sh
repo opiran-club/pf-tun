6ec1e4d6dcf7d8dcd85495b69cbad6ee62762c39
Opiran club <130220895+opiran-club@users.noreply.github.com>
2023-12-11 12:21:04 +0330

    Update pf-tun.sh

--- pf-tun.sh
+++ pf-tun.sh
@@ -2460,10 +2460,11 @@ chisel() {
 
 chisel_key() {
     key_path="/root/chisel_server.key"
-    chisel server --keygen ${key_path} > "${key_path}" &
+    chisel server --keygen ${key_path} &
     color green "Key was generated successfully at $key_path"
 }
 
+chisel server --keygen /root/chisel_server.key
     chisel_direct_kharej() {
         clear
         echo -e "${MAGENTA}Direct chisel tunnel (Server part) ${NC}"
