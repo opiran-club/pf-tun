27ee57e4bc0b6b049a0c1ec6732418aaf173de50
OPIran Official <130220895+opiran-club@users.noreply.github.com>
2024-01-11 18:59:35 +0330

    Update pf-tun.sh

--- pf-tun.sh
+++ pf-tun.sh
@@ -234,6 +234,7 @@ read option
         echo 'export PATH="$PATH:/usr/bin/git"' >> ~/.bashrc
         source ~/.bashrc
         apt install curl -y && bash <(curl -s https://raw.githubusercontent.com/Azumi67/Rathole_reverseTunnel/main/install2.sh)
+	apt-get install golang-go -y
 	go get github.com/AlecAivazis/survey/v2
  	go get github.com/fatih/color
   	go get github.com/pkg/sftp
